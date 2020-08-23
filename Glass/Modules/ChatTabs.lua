1b1beffd0cee1eb023cce247992a3ae43c514d3f
SammyJames <SammyJames@users.noreply.github.com>
2020-08-22 22:42:26 -0700

    Add option to unlock window from chat tab (#49)
    * add option to unlock window from chat tab
    
    * slight code cleanup
    
    * strip trailing whitespace

--- Glass/Modules/ChatTabs.lua
+++ Glass/Modules/ChatTabs.lua
@@ -8,6 +8,7 @@ local LSM = Core.Libs.LSM
 -- luacheck: push ignore 113
 local CHAT_CONFIGURATION = CHAT_CONFIGURATION
 local CLOSE_CHAT_WINDOW = CLOSE_CHAT_WINDOW
+local UNLOCK_WINDOW = UNLOCK_WINDOW
 local C_Timer = C_Timer
 local ChatConfigFrame = ChatConfigFrame
 local CreateFont = CreateFont
@@ -196,6 +197,16 @@ function CT:OnEnable()
       info.text = CHAT_CONFIGURATION;
       info.func = function() ShowUIPanel(ChatConfigFrame); end;
       info.notCheckable = 1;
+      UIDropDownMenu_AddButton(info);
+
+      -- unlock
+      info = UIDropDownMenu_CreateInfo();
+      info.text = UNLOCK_WINDOW;
+      info.notCheckable = 1;
+      info.func = function()
+        local M = Core:GetModule("Mover");
+        M:Unlock();
+      end;
       UIDropDownMenu_AddButton(info);
     end, "MENU")
   end
